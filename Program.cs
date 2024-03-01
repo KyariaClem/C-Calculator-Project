a930d3069e65a9e604b801251f542b558f9fc06a
kyariaclemons <kyarialove@gmail.com>
2024-03-01 14:11:58 -0600

    created calucation of how many kids user wants, added calcuation method from current kids plus future kids based on user input

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices.Marshalling;
+
+Console.WriteLine("Welcome to Kyaria's C# Calculator!");
+
+//Create calcuation 
+// how many current kids
+Console.WriteLine("How many kids do you currently have?");
+
+string inputCurrentKids = Console.ReadLine();
+
+int currentKids = int.Parse(inputCurrentKids);
+
+// Kids user wants to have
+
+Console.WriteLine("How many more kids do you want to have ?");
+string inputFutureKids = Console.ReadLine();
+
+int futureKids = int.Parse(inputFutureKids);
+
+// total number of kids current and future
+int totalKids = currentKids + futureKids;
+Console.WriteLine($"WoW ! You have {currentKids} and you want {futureKids}, you want a total of {totalKids} kids");
+
+switch(totalKids)
+{
+    case < 0:
+    break;
+}
